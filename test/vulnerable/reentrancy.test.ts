6d9ee0587c7a8e99e5e9c3ac312663a666788492
keisukefunatsu <keisukeyan2012@gmail.com>
2022-12-16 19:58:17 +0000

    add reentrancy test

--- test/vulnerable/reentrancy.test.ts
+++ test/vulnerable/reentrancy.test.ts
@@ -1,20 +1,43 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
 import { expect } from "chai"
+import { BigNumber } from "ethers"
+import { parseEther } from "ethers/lib/utils"
 import { ethers } from "hardhat"
 
 describe("Deployment", function () {
   const deployFixture = async () => {
-    const SimpleDAOFactory = await ethers.getContractFactory('SimpleDAO')
-    const SimpleDAO = await SimpleDAOFactory.deploy()
-    const MalloryDAOFactory = await ethers.getContractFactory('Mallory')
-    const Mallory = await MalloryDAOFactory.deploy()
-    await Mallory.deployed()
-    return { SimpleDAO, Mallory }
+    const VictimContractFactory = await ethers.getContractFactory('Victim')
+    const VictimContract = await VictimContractFactory.deploy()
+    const AttackerContractFactory = await ethers.getContractFactory('Attacker')
+    const AttackerContract = await AttackerContractFactory.deploy(VictimContract.address)
+    return { VictimContract, AttackerContract }
   }
   describe("reentrancy attack", function () {
     it("Should withdraw many times", async () => {
-      const { SimpleDAO, Mallory } = await loadFixture(deployFixture)
+      const { VictimContract, AttackerContract } = await loadFixture(deployFixture)
+      const [attackerSigner] = await ethers.getSigners()
+      let tx = await VictimContract.connect(attackerSigner).addBalance({
+        value: parseEther("10")
+      })
+      // const s = await addBalanceTx.wait()
+      // s.events?.filter((x) => {
+      //   x.event == "CurrentBalance" ? console.log(x) : "test"
+      // })
+      const balance = await VictimContract.getBalance(attackerSigner.address)
+      expect(balance).to.equal(BigNumber.from("10000000000000000000"))
 
+      let contractBalance = await ethers.provider.getBalance(VictimContract.address)
+      expect(contractBalance).to.equal(parseEther("10"))
+
+      // Attack
+      tx = await AttackerContract.connect(attackerSigner).attack({
+        value: parseEther("1")
+      })
+      await tx.wait()
+      contractBalance = await ethers.provider.getBalance(VictimContract.address)
+      expect(contractBalance).to.equal(BigNumber.from("0"))
+      contractBalance = await ethers.provider.getBalance(AttackerContract.address)
+      expect(contractBalance).to.equal(parseEther("11"))
     })
   })
 })
